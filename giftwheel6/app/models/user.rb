821cd5447ea90081e8a7fc055ec8c8b0932e954a
Liah Hansen <liahhansen@gmail.com>
2009-12-09 12:12:41 -0800

    model tests

--- giftwheel6/app/models/user.rb
+++ giftwheel6/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ActiveRecord::Base
+  validates_presence_of :first_name
+  validates_presence_of :last_name
+  validates_presence_of :position
+  validates_presence_of :wish_list
+  validates_presence_of :image
 end
--- giftwheel6/spec/models/user_spec.rb
+++ giftwheel6/spec/models/user_spec.rb
@@ -1,46 +1,47 @@
-require File.dirname(__FILE__) + "/../spec_helper"
-describe User do
-  before do
-    @user = User.new
-    @mediamaid.mediamaid_file_name = "sample.gif"
-   end
-  
-  it "should be invalid if mediamaid file name is nil" do
-    @mediamaid.mediamaid_file_name = ""
-    @mediamaid.should_not be_valid
+require File.dirname(__FILE__) + '/../spec_helper'
+module UserSpecHelper
+  def valid_user_attributes
+    { :id => 999999, 
+      :first_name => 'Marge',
+      :last_name => 'Simpson',
+      :position => '1', 
+      :wish_list => "Spa Certificate", 
+      :image => "marge.jpg"  }
   end
-  
-  it "should upload gifs" do
-    @mediamaid.mediamaid_content_type = "image/gif"
-    @mediamaid.should be_valid
-  end
+end
 
-  it "should upload jpegs" do
-    @mediamaid.mediamaid_content_type = "image/jpeg"
-    @mediamaid.should be_valid
+
+describe User do
+  include UserSpecHelper
+  before do
+      @user = User.new
+   end
+
+  specify "should be invalid without a first_name" do  
+    @user.attributes = valid_user_attributes.except(:first_name) 
+    @user.should_not be_valid
+    @user.first_name = 'Marge'
+    @user.should be_valid
   end
   
-  it "should upload pdf" do
-    @mediamaid.mediamaid_content_type = "application/pdf"
-    @mediamaid.should be_valid
+  specify "should be invalid without a last_name" do
+    @user.attributes = valid_user_attributes.except(:last_name) 
+    @user.should_not be_valid
+    @user.last_name = 'Simpson'
+    @user.should be_valid
   end
   
-  it "should upload png" do
-    @mediamaid.mediamaid_content_type = "image/png"
-    @mediamaid.should be_valid
+  specify "should be invalid without a position" do
+    @user.attributes = valid_user_attributes.except(:position) 
+    @user.should_not be_valid
+    @user.position = '1'
+    @user.should be_valid
   end
   
-  it "should upload flash" do
-    @mediamaid.mediamaid_content_type = "application/x-shockwave-flash"
-    @mediamaid.should be_valid
+  specify "should be invalid without a wish_list" do
+    @user.attributes = valid_user_attributes.except(:wish_list) 
+    @user.should_not be_valid
+    @user.wish_list = 'Spa Certificate'
+    @user.should be_valid
   end
-  
-  
-  it "should be invalid if mediamaid content type is mp3" do
-    @mediamaid.mediamaid_content_type = "audio/mp3"
-    @mediamaid.should_not be_valid
-  end
-  
-  
-    
 end
